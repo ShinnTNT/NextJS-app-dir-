62bda081ba7dfd418f8c48708e9a13945957fec2
shinnthant2004 <shinthantequi@gmail.com>
2023-09-08 09:57:56 +0630

    layout structure

---
+++ app/about/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const meta = {
+  title: "About Traversy Media",
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Traversy Media</h1>
+      <p>
+        {" "}
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum iusto esse
+        ab, a porro aliquid eos quasi, ipsam expedita ad nostrum sit, ut beatae.
+        Doloribus soluta dolores enim et unde?
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit magnam
+        dolorem, placeat nihil fugiat ab obcaecati sequi assumenda dolor
+        adipisci eius beatae neque facilis nesciunt laborum quos repellat fuga
+        nisi!
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
